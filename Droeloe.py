d80d2429d1ea16175c93a3d628dc65fe46f94a42
strNophix <nickpoelson@gmail.com>
2017-05-03 17:01:46 +0200

    Made it fancier, changed some lil issues

--- Droeloe.py
+++ Droeloe.py
@@ -2,15 +2,35 @@ import random
 
 class Droeloe:
     def __init__(self):
+        #Battle
         self.enemyhealth = random.randrange(8, 15)
         self.enemydamage = random.randrange(1, 6)
-        self.player_health = 20
         self.playerdamage = random.randrange(0, 6)
         self.turn = True
 
+        #Stats
+        self.currentlevel = 5
+        self.player_health = 20
+        self.currentprogress = 0
+        self.levelup = 100
+        self.attackpower = 4
+        self.defensepower = 4
+    
+    def Stats(self):
+        print("--------------------------------------------------------------")
+        print(" - Stats -")
+        print("Level:", self.currentlevel)
+        print("Attackpower add:", self.attackpower)
+        print("Defensepower add", self.defensepower)
+        print("Progress:", self.currentprogress)
+        print("--------------------------------------------------------------")
+        self.Turn()
+
     def Start(self):
-        print(self.enemyhealth)
-        print(self.player_health)
+        print("--------------------------------------------------------------")
+        print("Enemy has:", self.enemyhealth, "lives")
+        print("You have:", self.player_health, "lives")
+        print("--------------------------------------------------------------")
         self.Turn()
 
     def Turn(self):
@@ -23,11 +43,13 @@ class Droeloe:
             self.Atk()
 
     def Moves(self):
-        doing = str.lower(input("1. Attack, 2. Defend, 3. Run:  "))
+        doing = str.lower(input("1. Attack, 2. Defend, 3. Stats, 4. Run: "))
         if doing == "attack":
             self.Atk()
         elif doing == "defense":
             self.Dfnd()
+        elif doing == "stats":
+            self.Stats()
         elif doing == "run":
             print("Running away")
             exit
@@ -48,21 +70,22 @@ class Droeloe:
                 print("You dealt", self.playerdamage, "damage")
                 print("Enemy has", self.enemyhealth, "lives left")
                 self.turn = False
-                self.Moves()
+                self.Atk()
         elif self.turn == False:
             if self.player_health < 1:
+                print("--------------------------------------------------------------")
                 print("You lost")
+                print("--------------------------------------------------------------")
             else:
                 self.player_health = self.player_health - self.enemydamage
+                print("--------------------------------------------------------------")
                 print("Enemy dealt", self.enemydamage)
                 print("You got", self.player_health, "left")
+                print("--------------------------------------------------------------")
                 self.turn = True
                 self.Moves()
         else:
             self.Start()
-
-    def Term(self):
-        print(self.enemyhealth)
 
 Game = Droeloe()
 Game.Start()
